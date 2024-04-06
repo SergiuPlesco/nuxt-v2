e55cd7ae9b88d0f8f96776677239da2d62207ab5
Sergiu Plesco <plescosergiu@gmail.com>
2024-04-06 19:56:57 +0300

    removed path to axios config in plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,7 @@ export default {
   css: ["@/assets/css/style.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["~/plugins/axios.client.js"],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
